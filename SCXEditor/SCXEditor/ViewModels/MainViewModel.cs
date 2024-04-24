34dc7da28b35b8dd06de8c045fcc6200318860b5
Dylannichols702 <98860760+Dylannichols702@users.noreply.github.com>
2024-04-23 20:57:10 -0600

    Added New Chart Menu

--- SCXEditor/SCXEditor/ViewModels/MainViewModel.cs
+++ SCXEditor/SCXEditor/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Record_Book_MVVM.Commands;
 using SCXEditor.Models;
+using SCXEditor.Views.Windows;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,17 +14,30 @@ namespace SCXEditor.ViewModels
     {
 
         public ICommand ShowNewChartSetWindowCommand { get; set; }
+        public ICommand ShowNewChartWindowCommand { get; set; }
 
         public MainViewModel() { 
-            ShowNewChartSetWindowCommand = new RelayCommand(ShowWindow, CanShowWindow);
+            ShowNewChartSetWindowCommand = new RelayCommand(ShowChartSetWindow, CanShowChartSetWindow);
+            ShowNewChartWindowCommand = new RelayCommand(ShowChartWindow, CanShowChartWindow);
         }
 
-        private bool CanShowWindow(object obj)
+        private bool CanShowChartWindow(object obj)
         {
             return true;
         }
 
-        private void ShowWindow(object obj)
+        private void ShowChartWindow(object obj)
+        {
+            NewChart newChartWin = new NewChart();
+            newChartWin.Show();
+        }
+
+        private bool CanShowChartSetWindow(object obj)
+        {
+            return true;
+        }
+
+        private void ShowChartSetWindow(object obj)
         {
             NewChartSet newChartSetWin = new NewChartSet();
             newChartSetWin.Show();
