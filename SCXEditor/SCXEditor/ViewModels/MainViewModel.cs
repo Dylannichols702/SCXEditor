6c98e64a4ae939b1d7cc698be1cc644738c56ef1
Dylannichols702 <98860760+Dylannichols702@users.noreply.github.com>
2024-04-25 11:30:10 -0600

    Added remaining placeholder UI

--- SCXEditor/SCXEditor/ViewModels/MainViewModel.cs
+++ SCXEditor/SCXEditor/ViewModels/MainViewModel.cs
@@ -12,15 +12,67 @@ namespace SCXEditor.ViewModels
 {
     internal class MainViewModel
     {
-
+        // TODO: Refactor this logic to only require one ICommand
         public ICommand ShowNewChartSetWindowCommand { get; set; }
         public ICommand ShowNewChartWindowCommand { get; set; }
         public ICommand ShowLoadChartWindowCommand { get; set; }
+        public ICommand ShowLoadChartSetWindowCommand { get; set; }
+        public ICommand ShowChartSetPropertiesWindowCommand { get; set; }
+        public ICommand ShowChartPropertiesWindowCommand { get; set; }
+        public ICommand ShowModsMenuWindowCommand { get; set; }
 
         public MainViewModel() { 
             ShowNewChartSetWindowCommand = new RelayCommand(ShowChartSetWindow, CanShowChartSetWindow);
             ShowNewChartWindowCommand = new RelayCommand(ShowNewChartWindow, CanShowNewChartWindow);
             ShowLoadChartWindowCommand = new RelayCommand(ShowLoadChartWindow, CanShowLoadChartWindow);
+            ShowLoadChartSetWindowCommand = new RelayCommand(ShowLoadChartSetWindow, CanShowLoadChartSetWindow);
+            ShowChartSetPropertiesWindowCommand = new RelayCommand(ShowChartSetPropertiesWindow, CanShowChartSetPropertiesWindow);
+            ShowChartPropertiesWindowCommand = new RelayCommand(ShowChartPropertiesWindow, CanShowChartPropertiesWindow);
+            ShowModsMenuWindowCommand = new RelayCommand(ShowModsMenuWindow, CanShowModsMenuWindow);
+        }
+
+        private bool CanShowModsMenuWindow(object obj)
+        {
+            return true;
+        }
+
+        private void ShowModsMenuWindow(object obj)
+        {
+            ModsMenu modsMenuWin = new ModsMenu();
+            modsMenuWin.Show();
+        }
+
+        private bool CanShowChartPropertiesWindow(object obj)
+        {
+            return true;
+        }
+
+        private void ShowChartPropertiesWindow(object obj)
+        {
+            ChartProperties chartPropertiesWin = new ChartProperties();
+            chartPropertiesWin.Show();
+        }
+
+        private bool CanShowChartSetPropertiesWindow(object obj)
+        {
+            return true;
+        }
+
+        private void ShowChartSetPropertiesWindow(object obj)
+        {
+            ChartSetProperties chartSetPropertiesWin = new ChartSetProperties();
+            chartSetPropertiesWin.Show();
+        }
+
+        private bool CanShowLoadChartSetWindow(object obj)
+        {
+            return true;
+        }
+
+        private void ShowLoadChartSetWindow(object obj)
+        {
+            LoadChartSet loadChartSetWin = new LoadChartSet();
+            loadChartSetWin.Show();
         }
 
         private bool CanShowLoadChartWindow(object obj)
