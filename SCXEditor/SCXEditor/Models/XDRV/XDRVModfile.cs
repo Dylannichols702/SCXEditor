71cfbcd605babfa2b7d8a39a5a3368d57580f5b5
tari <76984137+tari-cat@users.noreply.github.com>
2024-04-25 12:32:34 -0700

    add xdrv models

---
+++ SCXEditor/SCXEditor/Models/XDRV/XDRVModfile.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+public class XDRVModfile
+{
+    public List<XDRVChartMod> chartMods;
+
+    public XDRVModfile(XDRV xdrv, string text)
+    {
+        chartMods = new List<XDRVChartMod>();
+
+        // do lua bullshit here
+    }
+}
