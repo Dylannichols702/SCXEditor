12f9af84ee38b7337c25db21466a327eb09e9d29
Dylannichols702 <98860760+Dylannichols702@users.noreply.github.com>
2024-04-26 11:13:18 -0600

    Begin migration to Avalonia Framework

--- SCXEditor/SCXEditor/Models/XDRV/XDRVDifficulty.cs
+++
@@ -1,8 +0,0 @@
-
-public enum XDRVDifficulty
-{
-    Beginner,
-    Normal,
-    Hyper,
-    Extreme
-}
---
+++ SCXEditor/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace SCXEditor.ViewModels;
+using ReactiveUI;
+using SCXEditor.Views;
+using System;
+using System.Reactive;
+
+public class MainViewModel : ViewModelBase
+{
+    public ReactiveCommand<Unit, Unit> ShowNewChartSetWindowCommand { get; set; }
+
+    public MainViewModel()
+    {
+        ShowNewChartSetWindowCommand = ReactiveCommand.Create(ShowNewChartSetWindow);
+    }
+
+    private void ShowNewChartSetWindow()
+    {
+        // TODO: Add logic for creating a new chart set directory
+    }
+}
