cebfd4e7c6dc77d73ea5995f84be3d8b2dee8f1d
nyhryan <nyhryan99102@gmail.com>
2024-05-05 23:57:20 +0900

    Added view model for ChartSetProperties and bind properties to it (except image file)

---
+++ SCXEditor/ViewModels/ChartSetPropertiesViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCXEditor.ViewModels
+{
+    public class ChartSetPropertiesViewModel : ViewModelBase
+    {
+        private int _Volume = 100;
+        private double _Offset = 0.00;
+
+        // how do I imagefile????
+        private String? _Illustrator;
+
+        public int Volume { get => _Volume; set => _Volume = value; }
+        public double Offset { get => _Offset; set => _Offset = value; }
+        public String? Illustrator { get => _Illustrator; set => _Illustrator = value; }
+
+        public ChartSetPropertiesViewModel()
+        {
+
+        }
+    }
+}
--- SCXEditor/Views/ChartSetProperties.axaml.cs
+++ SCXEditor/Views/ChartSetProperties.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using SCXEditor.ViewModels;
 
 namespace SCXEditor.Views;
 
@@ -9,5 +10,6 @@ public partial class ChartSetProperties : Window
     public ChartSetProperties()
     {
         InitializeComponent();
+        this.DataContext = new ChartSetPropertiesViewModel();
     }
 }
