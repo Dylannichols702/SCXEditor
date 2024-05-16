3dca54dcaa75df146584fe66b76b89825bad0e80
Dylannichols702 <98860760+Dylannichols702@users.noreply.github.com>
2024-05-16 15:10:44 -0600

    Added basic rendering functionality

--- SCXEditor/ChartRenderer/ChartRenderer.cs
+++ SCXEditor/ChartRenderer/ChartRenderer.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Avalonia.Controls.Primitives;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGame;
+using System.Collections.Generic;
+using SCXEditor.ChartRenderer.RendererElements;
 
 namespace SCXEditor.ChartRenderer;
 
 public class ChartRenderer : Game
 {
+    private List<RendererElement> _elements = new List<RendererElement>();
+
+    private RendererElements.Track _leftTrack = new(450, 0, 200, 1000);
+    private RendererElements.Track _rightTrack = new(700, 0, 200, 1000);
+
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
@@ -20,7 +28,8 @@ public class ChartRenderer : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
-
+        _elements.Add(_leftTrack);
+        _elements.Add(_rightTrack);
         base.Initialize();
     }
 
@@ -40,10 +49,14 @@ public class ChartRenderer : Game
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Microsoft.Xna.Framework.Color.CornflowerBlue);
+        GraphicsDevice.Clear(new Microsoft.Xna.Framework.Color(96, 77, 128));
 
         // TODO: Add your drawing code here
         _spriteBatch.Begin();
+        foreach (RendererElement element in _elements)
+        {
+            element.Draw(ref _spriteBatch);
+        }
         _spriteBatch.End();
 
         base.Draw(gameTime);
---
+++ SCXEditor/ChartRenderer/RendererElements/Track.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame;
+
+namespace SCXEditor.ChartRenderer.RendererElements
+{
+    public class Track : RendererElement
+    {
+        private Rectangle rect = new Rectangle();
+        Microsoft.Xna.Framework.Color color;
+
+        public int X
+        {
+            get { return rect.X; }
+            set { rect.X = value; }
+        }
+
+        public int Y
+        {
+            get { return rect.Y; }
+            set { rect.Y = value; }
+        }
+
+        public Track(int x, int y, int width, int height)
+        {
+            rect.X = x;
+            rect.Y = y;
+            rect.Width = width;
+            rect.Height = height;
+            color = new Microsoft.Xna.Framework.Color(0, 0, 0);
+        }
+
+        public void Draw(ref SpriteBatch spriteBatch)
+        {
+            spriteBatch.FillRectangle(rect, color);
+        }
+    }
+}
